95efb692165bb3dcec2b5940b672d3e3096f67b7
Eric <217014110@stu.vtc.edu.hk>
2022-06-06 22:48:50 +0800

    feat: registration form ui

---
+++ src/components/registrationform.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Form, Input, Button } from 'antd';
+import http from '../common/http-common'
+
+function onFinish(values) {
+  //console.log("Success", values)
+
+  const {confirm, ...data} = values;
+  http.post('/users', data)
+  .then((response)=>{
+    console.log(response.data)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
+
+const emailRules = [
+    {type: 'email', message: 'The email is not valid!'},
+    {required: true, message: 'Please input your E-mail!' }
+];
+
+const passwordRules = [
+    { required: true, message: 'Please enter your password!' }
+];
+
+const confirmRules = [
+    { required: true, message: 'Please confirm your password!' },
+    ({ getFieldValue }) => ({
+        validator(rule, value) {
+            if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject('Password enter mismatch!');
+        }
+    })
+];
+
+const usernameRules = [
+    { required: true, message: 'Please enter your username!', whitespace: true }
+]
+
+const formItemLayout = {
+  labelCol: { xs: { span: 24 }, sm: { span: 6 } },
+  wrapperCol: { xs: { span: 24 }, sm: { span: 12 } }
+};
+const tailFormItemLayout = {
+  wrapperCol: { xs: { span: 24, offset: 0 }, sm: { span: 16, offset: 6 } },
+};
+
+
+function RegistrationForm() {  
+  return (
+    <Form name="register" {...formItemLayout} scrollToFirstError onFinish={onFinish}>
+        
+        <Form.Item name="email" label="E-mail" rules={emailRules}>
+            <Input />
+        </Form.Item>
+
+        <Form.Item name="password" label="Password" rules={passwordRules} hasFeedback>
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="confirm" label="Confirm Password" rules={confirmRules} hasFeedback>
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="username" label="Username" rules={usernameRules}>
+            <Input />
+        </Form.Item>
+
+        <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+                Register
+            </Button>
+        </Form.Item>
+      </Form>
+
+  );    
+}  
+
+export default RegistrationForm;
